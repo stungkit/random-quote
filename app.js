eba1e1b9332b254d4c52457ea29b375d541d0c70
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-01-28 17:29:40 -0800

    Finished Writing a Service section.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 (function() {
+  var Class = ng.core.Class;
   var Component = ng.core.Component;
   var NgModule = ng.core.NgModule;
   var BrowserModule = ng.platformBrowser.BrowserModule;
   var PlatformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+
+  var QuoteService = Class({
+    constructor: function QuoteService(){
+      this.quotes = sampleQuotes;
+    },
+    getRandomQuote: function(){
+      var randomIndex = Math.floor(Math.random() * this.quotes.length);
+      return this.quotes[randomIndex];
+    }
+  });
 
   var RandomQuoteComponent = Component({
     selector: 'random-quote',
     template: '<p><em>{{quote["line"]}}</em> - {{quote["author"]}}</p>'
   })
   .Class({
-    constructor: function(){
-      var randomIndex = Math.floor(Math.random() * quotes.length);
-      this.quote = quotes[randomIndex];
-    }
+    constructor: [QuoteService, function RandomQuoteComponent(quoteService){
+      this.quote = quoteService.getRandomQuote();
+    }]
   });
 
   var AppComponent = Component({
@@ -22,12 +32,13 @@
     '<random-quote></random-quote>'
   })
   .Class({
-    constructor: function(){}
+    constructor: function AppComponent(){}
   })
 
   var AppModule = NgModule({
     imports: [BrowserModule],
     declarations: [AppComponent, RandomQuoteComponent],
+    providers: [QuoteService],
     bootstrap: [AppComponent]
   })
   .Class({
@@ -36,7 +47,7 @@
 
   PlatformBrowserDynamic().bootstrapModule(AppModule);
 
-  var quotes = [
+  var sampleQuotes = [
     {
       "line": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
       "author": "Brian W. Kernighan"
